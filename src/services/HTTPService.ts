b4552a2eb3ac28904a965a474e41fa4f0d7ee30b
Tibeau S <tibeau.schodts@gmail.com>
2019-06-23 13:33:26 +0200

    Update prodURL

--- src/services/HTTPService.ts
+++ src/services/HTTPService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const devURL: string = 'http://localhost:3200';
-const prodURL: string = 'https://stingrayy-backend.herokuapp.com/';
+const prodURL: string = 'https://opensea-api-converter.herokuapp.com/';
 
 export function getAssets() {
     return axios.get(prodURL).then(r => r.data);
